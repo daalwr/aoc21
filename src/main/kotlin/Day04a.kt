062da44bef0fce77f309865b9bb9ea37e714c022
Daniel Wright <Daniel.Wright@ingka.ikea.com>
2021-12-04 18:34:52 +0100

    day 4

---
+++ src/main/kotlin/Day04a.kt
@@ -0,0 +1,65 @@
+import java.io.File
+
+data class BingoCard(val numbers: List<List<Int>>) {
+
+    fun isWinner(winningNumbers: List<Int>): Boolean {
+        for (row in numbers) {
+            val markedNumbers = row.filter { number -> winningNumbers.contains(number) }
+            if (markedNumbers.size == 5) {
+                return true
+            }
+        }
+
+        for (column in 0 until 5) {
+            val columnNumbers = 0.until(5).map { row -> numbers[row][column] }
+            val markedNumbers = columnNumbers.filter { number -> winningNumbers.contains(number) }
+            if (markedNumbers.size == 5) {
+                return true
+            }
+        }
+
+        return false
+    }
+
+    fun sumOfUnmarkedNumbers(winningNumbers: List<Int>): Int {
+        return numbers.flatten().filter { x -> !winningNumbers.contains(x) }.sum()
+    }
+}
+
+
+fun main(args: Array<String>) {
+    val lines: List<String> = File("src/main/kotlin/input04.txt").readLines()
+
+    val allWinningNumbers = lines.first().split(",").map { x -> x.toInt() }
+    val bingoCardCount = (lines.size - 1) / 6
+    val bingoCards: MutableList<BingoCard> = mutableListOf()
+
+    for (bingoCardNumber in 0 until bingoCardCount) {
+
+        val bingoCardLines: MutableList<List<Int>> = mutableListOf()
+
+        for (rowNumber in 0 until 5) {
+            val fileLineNumber = 2 + bingoCardNumber * 6 + rowNumber
+            val line = lines[fileLineNumber]
+            val numberLine = line.trim().split(Regex("\\s+")).map { x -> x.toInt() }
+            bingoCardLines.add(numberLine)
+        }
+
+        val bingoCard = BingoCard(bingoCardLines.toList())
+        bingoCards.add(bingoCard)
+    }
+
+    for (i in 1 until allWinningNumbers.size) {
+        val chosenNumbers = allWinningNumbers.take(i)
+
+        for (bingoCard in bingoCards) {
+            if (bingoCard.isWinner(chosenNumbers)) {
+                val sumOfUnmarkedNumbers = bingoCard.sumOfUnmarkedNumbers(chosenNumbers)
+                val lastNumber = chosenNumbers.last()
+                println(sumOfUnmarkedNumbers * lastNumber)
+                return
+            }
+        }
+    }
+
+}
