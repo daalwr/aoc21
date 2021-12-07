bdb3e819da7987403789ffba4d2a69037d71362a
Daniel Wright <Daniel.Wright@ingka.ikea.com>
2021-12-07 06:51:40 +0100

    day 7

---
+++ src/main/kotlin/Day07b.kt
@@ -0,0 +1,29 @@
+import java.io.File
+import java.lang.Math.abs
+
+fun main(args: Array<String>) {
+    val lines: List<String> = File("src/main/kotlin/input07.txt").readLines()
+
+    val crabs = lines.first().split(",").map { it.toInt() }
+
+    val max = crabs.maxOrNull()!!.toInt()
+    val min = crabs.minOrNull()!!.toInt()
+
+    var minSteps = Int.MAX_VALUE
+
+    for (candidatePosition in min..max) {
+        var totalDistance = 0
+        for (crab in crabs) {
+            val distanceFromCandidate = abs(candidatePosition - crab)
+            totalDistance += distanceFromCandidate * (distanceFromCandidate + 1) / 2
+        }
+
+        if (totalDistance < minSteps) {
+            minSteps = totalDistance
+        }
+
+    }
+
+    println(minSteps)
+
+}
